1061df18153de4b1b8b97a8e1426ee92c7115ade
王飞平 <wfp19920821@163.com>
2018-02-09 16:00:56 +0800

    Update index.py

--- routes/index.py
+++ routes/index.py
@@ -1,18 +1,34 @@
-from flask import Blueprint
-from routes.forms import RegisterForm
-from flask import render_template
-
+from flask import (
+    Blueprint,
+    render_template,
+    session,
+    request,
+)
 
 main = Blueprint('index', __name__)
 
 
+# 根据session得到当前的用户
+def current_user():
+    cur = session.get('user_name', '')
+    return cur
+
+
 @main.route('/', methods=['GET', 'POST'])
+def index():
+    u = session.get('user_name', '')
+    # if u != 'wfp':
+    return render_template('login.html')
+    # return 'hello {}'.format(u)
+
+
+@main.route('/login', methods=['GET', 'POST'])
+def login():
+    u = request.form.get('name')
+    session['user_name'] = u
+    return '<h1>success</h1>'
+
+
+@main.route('/register', methods=['GET', 'POST'])
 def register():
-    form = RegisterForm()
-    if form.validate_on_submit():
-        print(form.name.data)
-        print(form.password.data)
-        
-    return render_template('index.html', form=form)
-    
-    
+    pass
