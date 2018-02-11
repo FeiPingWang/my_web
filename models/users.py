456c11ba9cdb154c30145d44476ffec3ae76ec7c
FeiPingWang <wfp19920821@163.com>
2018-02-11 22:14:40 +0800

    登录和注册功能完善
    完善了登录和注册功能，并启用了数据库Users表

--- models/users.py
+++ models/users.py
@@ -1,6 +1,7 @@
 from sqlalchemy import Column, Integer, String, DateTime
 from models import Base, new_session, Model, engine
 import datetime
+from flask import flash
 
 
 # 用户表
@@ -10,28 +11,38 @@ class User(Base):
     # id = Column(Integer, primary_key=True)
     name = Column(String(20))
     password = Column(String(20))
+    phone = Column(String(30))
     email = Column(String(30))
     brief = Column(String(140), default='新人报道啦')
     created_date = Column(DateTime, default=datetime.datetime.utcnow)
     
+    def __init__(self, form):
+        self.name = form.get('username', '')
+        self.password = form.get('password', '')
+        self.email = form.get('email', '')
+        self.phone = form.get('phone', '')
+        self.brief = form.get('brief', '')
+
     def __repr__(self):
         return "<User(id='{}', name='{}', email='{}', brief='{}')>".format(
             self.id, self.name, self.email, self.brief
         )
     
+    @classmethod
+    def validate(cls, name, password):
+        ret = True
+        session = new_session()
+        result = session.query(User.name, User.password).filter(User.name==name, User.password==password).first()
+        if result == None:
+            ret = False
+        session.close()
+        return ret
         
+
 if __name__ == '__main__':
+    # Base.metadata.create_all(engine)
+    session = new_session()
+    u = session.query(User.name, User.password).filter(User.name == 'wfp', User.password == '1234').first()
+    print(u)
     pass
-    # # Base.metadata.create_all(engine)
-    # session = new_session()
-    # u1 = User(name='wfp', password='123456', email='wfp19920821@163.com')
-    # # # p = session.query(User).filter(User.name == 'pxx')
-    # # # # session.close()
-    # # # d = dict(
-    # # #     name = 'pxx',
-    # # #     email = '1234',
-    # # # )
-    # # # p.delete()
-    # session.add(u1)
-    # session.commit()
     
--- routes/index.py
+++ routes/index.py
@@ -3,7 +3,11 @@ from flask import (
     render_template,
     session,
     request,
+    redirect,
+    url_for,
+    flash,
 )
+from models.users import User
 
 main = Blueprint('index', __name__)
 
@@ -17,18 +21,28 @@ def current_user():
 @main.route('/', methods=['GET', 'POST'])
 def index():
     u = session.get('user_name', '')
-    # if u != 'wfp':
-    return render_template('login.html')
-    # return 'hello {}'.format(u)
+    return 'hello {}'.format(u)
 
 
 @main.route('/login', methods=['GET', 'POST'])
 def login():
-    u = request.form.get('name')
-    session['user_name'] = u
-    return '<h1>success</h1>'
+    if request.method == 'POST':
+        name = request.form.get('username', '')
+        password = request.form.get('password', '')
+        if User.validate(name, password):
+            session['user_name'] = name
+            return redirect(url_for('index.index')) # 登录成功
+        else:
+            flash('找不到用户名')
+    return render_template('login.html')
 
 
 @main.route('/register', methods=['GET', 'POST'])
 def register():
-    pass
+    if request.method == 'POST':
+        form = request.form
+        u = User(form)
+        User._add(u)
+        return redirect(url_for('index.index'))
+    return render_template('register.html')
+
